d178962df2f8e11ec33fcd3ba403742c5170b035
Prashant <prashant78907890@gmail.com>
2026-04-18 16:09:47 +0530

    Added the full screen in and the button to apply on wallpaper

--- lib/views/full_screen.dart
+++ lib/views/full_screen.dart
@@ -1,9 +1,50 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import 'package:wallpaper_app/models/photo_model.dart';
 
 class FullScreen extends StatelessWidget {
   final PhotoModel photo;
   const FullScreen({super.key, required this.photo});
+
+  Future<void> setWallpaper(BuildContext context) async {
+    try {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Processing Image...")));
+
+      var response = await http.get(Uri.parse(photo.url));
+
+      Directory directory = await getTemporaryDirectory();
+      String filePath = '${directory.path}/my_wallpaper.jpg';
+
+      File file = File(filePath);
+
+      await file.writeAsBytes(response.bodyBytes);
+
+      int location = WallpaperManager.HOME_SCREEN;
+
+      bool result = await WallpaperManager.setWallpaperFromFile(
+        filePath,
+        location,
+      );
+
+      if (!context.mounted) return;
+
+      if (result) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text("Wallpaper Updated!")));
+      }
+    } catch (e) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error: $e")));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +52,37 @@ class FullScreen extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
 
-      body: Hero(
-        tag: photo.id,
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Image.network(photo.url, fit: BoxFit.cover),
-        ),
+      body: Stack(
+        children: [
+          Hero(
+            tag: photo.id,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.network(photo.url, fit: BoxFit.cover),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            left: 0,
+            right: 0,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white.withOpacity(0.8),
+                foregroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onPressed: () => setWallpaper(context), // Call the function
+              child: Text(
+                "Set as Wallpaper",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
